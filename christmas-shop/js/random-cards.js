31d73e6845164a0d8aa281f73030ff82f7a1e3bb
roman <romanusovich1@gmail.com>
2026-04-18 13:33:14 +0200

    feat: add random cars functionality

---
+++ christmas-shop/js/random-cards.js
@@ -0,0 +1,41 @@
+async function loadGiftsJSON() {
+    return fetch('js/gifts.json')
+        .then(response => response.json())
+        .catch(error => {
+            console.error('Error loading gifts.json:', error);
+            return [];
+        });
+}
+
+function getRandomGifts(gifts, count) {
+    const shuffled = [...gifts].sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, count);
+}
+
+const giftsImages = {
+    'For Work': 'images/gift-for-work.png',
+    'For Health': 'images/gift-for-health.png',
+    'For Harmony': 'images/gift-for-harmony.png', 
+}
+
+function displayRandomGifts(randomGifts) {
+    const bestGiftItems = document.querySelectorAll('.best-gift-item');
+    bestGiftItems.forEach((item, index) => {
+        if (randomGifts[index]) {
+            const gift = randomGifts[index];
+            item.querySelector('img').src = giftsImages[gift.category];
+            item.querySelector('.h4').textContent = gift.category;
+            item.querySelector('.h4').classList.add(gift.category.toLowerCase().replace(' ', '-'));
+            item.querySelector('.h3').textContent = gift.name;
+        } else {
+            item.style.display = 'none';
+        }  
+  });
+}
+
+loadGiftsJSON().then(gifts => {
+    if (gifts.length > 0) {
+        const randomGifts = getRandomGifts(gifts, 4);
+        displayRandomGifts(randomGifts);
+    }
+});
